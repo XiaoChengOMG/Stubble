29f6e65ff573ab5bb066306d293e1cfe1c1fbadb
XiaoChengOMG <444059482@qq.com>
2016-09-18 14:45:54 +0800

    spider baidubaike

---
+++ html_outputer.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+'''
+Created on 2016年9月8日
+
+@author: Administrator
+'''
+class HtmlOutputer(object):
+    
+    def __init__(self):
+        self.datas = []
+    
+    def collect_data(self,data):
+        if data is None:
+            return
+        self.datas.append(data)
+    
+    def output_html(self):
+        fout = open('output.html','w')
+        fout.write("<html>")
+        fout.write("<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>")
+        fout.write("<body>")
+        fout.write("<table>")
+        
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode("utf-8"))
+            fout.write("<td>%s</td>" % data['summary'].encode("utf-8"))
+            fout.write("</tr>")
+            
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
